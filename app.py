8072438f65811dd9cc41ca9e843f69f700f361eb
rkrsinu <152066159+rkrsinu@users.noreply.github.com>
2026-03-06 12:05:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,17 @@ st.title("Co(II) Magnetic Property Predictor")
 
 uploaded_file = st.file_uploader("Upload XYZ file", type=["xyz"])
 
+
 if uploaded_file:
 
     atoms,coords = read_xyz(uploaded_file)
 
-    co_index, donors = find_donors(atoms,coords)
+    co_index, donors, message = find_donors(atoms,coords)
+
+    if message:
+
+        st.warning(message)
+        st.stop()
 
     donor_indices=[d[0] for d in donors]
 
@@ -23,24 +29,24 @@ if uploaded_file:
 
     st.subheader("Detected donor atoms")
 
-    donor_display=[]
+    donor_table=[]
 
-    for idx,d in enumerate(donor_indices):
+    for i,d in enumerate(donor_indices):
 
-        donor_display.append({
-            "Donor atom index":d+1,
-            "Atom":atoms[d],
-            "Bond length (Å)":round(BL[idx],3)
+        donor_table.append({
+        "Donor atom index": d+1,
+        "Atom": atoms[d],
+        "Co–L bond length (Å)": round(BL[i],3)
         })
 
-    st.table(pd.DataFrame(donor_display))
+    st.table(pd.DataFrame(donor_table))
 
     confirm = st.radio("Are these donor atoms correct?",["Yes","No"])
 
     if confirm=="No":
 
         manual = st.text_input(
-        "Enter donor atom indices separated by comma (example: 10,25,41)")
+        "Enter donor atom indices separated by comma (example: 12,34,56)")
 
         if manual:
 
@@ -48,49 +54,37 @@ if uploaded_file:
 
             BL,BA = compute_descriptors(coords,co_index,donor_indices)
 
-            st.write("Updated bond lengths:")
-
-            st.write(BL)
+            st.write("Updated bond lengths (Å):",BL)
 
             confirm2 = st.radio("Proceed with prediction?",["Yes","No"])
 
-            if confirm2=="Yes":
-                proceed=True
-            else:
-                proceed=False
-        else:
-            proceed=False
+            if confirm2!="Yes":
+                st.stop()
 
-    else:
-        proceed=True
+    X = np.array([[BL[0],BL[1],BL[2],BA[0],BA[1],BA[2]]])
 
+    model_D = joblib.load("models/GB_model_D.joblib")
+    model_ED = joblib.load("models/GB_model_E_D.joblib")
+    model_gx = joblib.load("models/GB_model_gx.joblib")
+    model_gy = joblib.load("models/GB_model_gy.joblib")
+    model_gz = joblib.load("models/GB_model_gz.joblib")
 
-    if proceed:
+    D = model_D.predict(X)[0]
+    ED = model_ED.predict(X)[0]
+    gx = model_gx.predict(X)[0]
+    gy = model_gy.predict(X)[0]
+    gz = model_gz.predict(X)[0]
 
-        X = np.array([[BL[0],BL[1],BL[2],BA[0],BA[1],BA[2]]])
+    st.subheader("Predicted Magnetic Parameters")
 
-        model_D = joblib.load("models/GB_model_D.joblib")
-        model_ED = joblib.load("models/GB_model_E_D.joblib")
-        model_gx = joblib.load("models/GB_model_gx.joblib")
-        model_gy = joblib.load("models/GB_model_gy.joblib")
-        model_gz = joblib.load("models/GB_model_gz.joblib")
+    results = pd.DataFrame({
+        "Parameter":["D","E/D","gx","gy","gz"],
+        "Value":[round(D,3),round(ED,4),round(gx,3),round(gy,3),round(gz,3)]
+    })
 
-        D = model_D.predict(X)[0]
-        ED = model_ED.predict(X)[0]
-        gx = model_gx.predict(X)[0]
-        gy = model_gy.predict(X)[0]
-        gz = model_gz.predict(X)[0]
+    st.table(results)
 
-        st.subheader("Predicted Magnetic Parameters")
-
-        results = pd.DataFrame({
-            "Parameter":["D","E/D","gx","gy","gz"],
-            "Value":[round(D,3),round(ED,4),round(gx,3),round(gy,3),round(gz,3)]
-        })
-
-        st.table(results)
-
-        st.markdown(
-        "For more details visit: "
-        "[https://doi.org/10.26434/chemrxiv-2024-97555](https://doi.org/10.26434/chemrxiv-2024-97555)"
-        )
+    st.markdown(
+    "For more details visit: "
+    "[https://doi.org/10.26434/chemrxiv-2024-97555](https://doi.org/10.26434/chemrxiv-2024-97555)"
+    )
